e66dc295cc2427652c68cec356b3c64a557cca4b
Ganeshrockz <ganeshrockz@github.com>
2020-12-23 11:55:30 +0530

    Added build

--- src/greet.ts
+++ src/greet.ts
@@ -8,4 +8,22 @@ export class GreetMe {
     public greet() {
         return "Hello " + this.name; 
     }
+
+    public greetBasedOnTime() {
+        let date = new Date();
+        let hour = date.getHours();
+        let greetMessage = "";
+
+        if (hour < 12) {
+            greetMessage = "Good morning ";
+        }
+        else if (hour >= 12 && hour <=17) {
+            greetMessage = "Good noon ";
+        }
+        else {
+            greetMessage = "Good night ";
+        }
+
+        return greetMessage + this.name;
+    }
 }
