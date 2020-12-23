be5b1e5090a9e38f5897901887928660db20f4ea
Ganeshrockz <ganeshrockz@github.com>
2020-12-23 21:46:01 +0530

    Added export file

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { GreetMe } from "./greet";
+import { A, B, C, D} from "./models";
+
+export {
+    GreetMe,
+    A,
+    B,
+    C,
+    D
+}
---
+++ src/models.ts
@@ -0,0 +1,16 @@
+export class A {
+    type: string;
+}
+
+export class B {
+    provider: string;
+    ds: D[];
+}
+
+export class C {
+    repository: string;
+}
+
+export class D {
+    id: string;
+}
